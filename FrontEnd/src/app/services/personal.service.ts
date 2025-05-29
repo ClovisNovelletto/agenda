5e93a30b787e36374cee9e33fc3ad63af9193db7
Clóvis Novelletto <clovis.novelletto@gmail.com>
2025-05-29 19:31:10 -0300

    ajuste rota persoanl service

--- FrontEnd/src/app/services/personal.service.ts
+++ FrontEnd/src/app/services/personal.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Personal } from '../models/personal.model';
 import { HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../src/environments/environment';
 
 interface ConfigPersonal {
   diasAtendimento: number[];
@@ -35,13 +36,13 @@ export class PersonalService {
   salvarConfiguracoes(config: ConfigPersonal): Observable<any> {
     const token = localStorage.getItem('jwt-token');
     const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
-    return this.http.put('/api/personal/configuracoes', config, { headers });
+    return this.http.put(`${environment.apiUrl}/personal/configuracoes`, config, { headers });
   }
-
+    
   getMe(): Observable<Personal> {
     const token = localStorage.getItem('jwt-token');
     const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
-    return this.http.get<Personal>('/api/personal/me', { headers });
+    return this.http.get<Personal>(`${environment.apiUrl}/personal/me`, { headers });
     //return this.http.get<Personal>('/api/personal/me');
   }
 }
