239da411ddf01b9cdfd43068a28a93ee0c904db1
Clóvis Novelletto <clovis.novelletto@gmail.com>
2025-11-05 10:56:55 -0300

    tratamento envio email HTTP Resend

--- Backend/mailer.js
+++ Backend/mailer.js
@@ -1,4 +1,25 @@
 // mailer.js
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendMail(to, subject, html) {
+  try {
+    const data = await resend.emails.send({
+      from: 'h2u Agenda <no-reply@h2uagenda.com.br>', // pode mudar o domínio depois
+      to,
+      subject,
+      html,
+    });
+    console.log('E-mail enviado com sucesso:', data);
+  } catch (error) {
+    console.error('Erro ao enviar e-mail:', error);
+    throw new Error('Falha ao enviar e-mail');
+  }
+}
+
+
+/*
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
@@ -17,3 +38,4 @@ export async function sendMail(to, subject, html) {
     html,
   });
 }
+*/
