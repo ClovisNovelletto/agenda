78ae6ada1228ab61235c247e42b9d0819ed12c86
Clóvis Novelletto <clovis.novelletto@gmail.com>
2026-04-02 17:36:22 -0300

    teste tamanho

--- FrontEnd/src/app/ajuda/ajuda.component.ts
+++ FrontEnd/src/app/ajuda/ajuda.component.ts
@@ -5,7 +5,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-ajuda',
-  imports: [MatDialogContent, MatDialogActions],
+  imports: [MatDialogActions],
   templateUrl: './ajuda.component.html',
   styleUrls: ['./ajuda.component.css']
 })
