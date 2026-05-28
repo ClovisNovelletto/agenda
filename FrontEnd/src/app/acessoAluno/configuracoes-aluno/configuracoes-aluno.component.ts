4776ec159451553b80e74ab1248d3545c1ba8e68
Clóvis Novelletto <clovis.novelletto@gmail.com>
2026-05-27 22:13:45 -0300

    dados aluno salvar

--- FrontEnd/src/app/acessoAluno/configuracoes-aluno/configuracoes-aluno.component.ts
+++ FrontEnd/src/app/acessoAluno/configuracoes-aluno/configuracoes-aluno.component.ts
@@ -18,10 +18,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { MatOptionModule } from '@angular/material/core';
 import { MatFormField } from '@angular/material/form-field';
-import { Personal } from '../../models/personal.model';
-import { PersonalService } from '../../services/personal.service';
 import { Observable } from 'rxjs';
-import { ConfigAgenda } from '../../models/configAgenda.model';
 import { map } from 'rxjs/operators';
 import { FormArray, FormControl } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -53,55 +50,13 @@ export class ConfiguracoesAlunoComponent implements OnInit {
   aluno: Aluno | null = null;
 
   mensagem: string ='';
-  horasPossiveis: string[] = [];
   intervalo: number = 10; // valor padrão
   horaInicio: number = 6; // valor padrão
   horaFim: number = 22; // valor padrão
-  personal?:  Personal;
   diasSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
-  configAgenda: ConfigAgenda = {
-    diasAtendimento: [],
-    horaInicio: 8,
-    horaFim: 18,
-    intervaloMinutos: 10,
-    mostrarLocal: true, 
-    mostrarServico: true,
-    mostrarEquipto: true,
-    servicoid: 23,
-  };
-
-  /*Plano = duração contrato*/
-  planosxxx = [
-    { id: 1, nome: 'Experimental' },
-    { id: 2, nome: 'Avulso' },
-    { id: 3, nome: 'Mensal' },
-    { id: 4, nome: 'Bimestral' },
-    { id: 5, nome: 'Trimestral' },
-    { id: 6, nome: 'Quadrimestral' },
-    { id: 7, nome: 'Semestral' },
-    { id: 8, nome: 'Anual' },
-  ];
-
-  /* frequencia = aula por período*/
-  frequenciasxxx = [
-    { id: 1, nome: '1x por Semana' },
-    { id: 2, nome: '2x por Semana' },
-    { id: 3, nome: '3x por Semana' },
-    { id: 4, nome: '4x por Semana' },
-    { id: 5, nome: '5x por Semana' },
-    { id: 6, nome: '1x por Quinzena' },
-    { id: 7, nome: '1x por Mês' },
-    { id: 8, nome: '1x por Bimestre' },
-    { id: 9, nome: '1x por Trimestre' },
-    { id: 10, nome: '1x por Quadrimestre' },
-    { id: 11, nome: '1x por Semestre' },
-    { id: 12, nome: '1x por Ano' },
-  ];
-
 
   constructor(
     private fb: FormBuilder,
-    private personalService: PersonalService,
     private alunoService: AlunoService, private snackBar: MatSnackBar,
     private http: HttpClient, private cd: ChangeDetectorRef,
   ) {}
@@ -125,31 +80,10 @@ export class ConfiguracoesAlunoComponent implements OnInit {
     });
 
     forkJoin({
-      config: this.loadPersonal(),
       aluno: this.getDadosAluno(),
-    }).subscribe(({ config, aluno }) => {
-      this.configAgenda = config;
+    }).subscribe(({ aluno }) => {
       this.aluno = aluno;
       
-      // --- Inicializa autocomplete ---
-      this.horasPossiveis = this.gerarHorasPossiveis(this.configAgenda.horaInicio, this.configAgenda.horaFim, this.configAgenda.intervaloMinutos);
-
-      console.log('horasPossiveis', this.horasPossiveis);
-      // --- Inicializa formArray diasAula ---
-      const formArray = this.form.get('diasAula') as FormArray;
-      for (let i = 0; i < 7; i++) {
-        const estaHabilitado = this.configAgenda.diasAtendimento.includes(i);
-        const grupo = this.fb.group({
-          ativo: [{ value: false, disabled: !estaHabilitado }],
-          hora: [{ value: '', disabled: true }]
-        });
-        grupo.get('ativo')?.valueChanges.subscribe(ativo => {
-          const horaControl = grupo.get('hora');
-          ativo && estaHabilitado ? horaControl?.enable() : horaControl?.disable();
-        });
-        formArray.push(grupo);
-      }
-
       if (this.aluno?.id) {
 
 
@@ -170,6 +104,17 @@ export class ConfiguracoesAlunoComponent implements OnInit {
         this.form.get('ativo')?.disable();
         // Dias de aula
         const diasAulaArray = this.form.get('diasAula') as FormArray;
+
+        for (let i = 0; i < 7; i++) {
+          diasAulaArray.push(
+            this.fb.group({
+              ativo: [false],
+              hora: ['']
+            })
+          );
+        }
+
+        console.log('diasAulaArray', diasAulaArray)
         for (let i = 0; i < 7; i++) {
 
           const diaKey = `aludia${i}` as keyof typeof this.aluno;
@@ -181,18 +126,18 @@ export class ConfiguracoesAlunoComponent implements OnInit {
 
           const grupo = diasAulaArray.at(i);
 
+          console.log(i, grupo);
           grupo.get('ativo')?.setValue(ativo);
           grupo.get('hora')?.setValue(hora);
 
-          console.log('ativo', ativo);
-          console.log('hora', hora);
-          console.log('grupo', grupo);
+          // impede editar o toggle
+          grupo.get('ativo')?.disable();
+
           ativo
             ? grupo.get('hora')?.enable()
             : grupo.get('hora')?.disable();
         }
       }
-
 
       // --- Marcar campos como dirty para habilitar botão salvar ---
       this.form.markAllAsTouched();
@@ -233,7 +178,7 @@ export class ConfiguracoesAlunoComponent implements OnInit {
             horizontalPosition: 'center',
             verticalPosition: 'top'
           });
-
+          this.form.markAsPristine();
         },
         error: () => {
           this.mensagem = '❌ Erro ao salvar dados.';
@@ -252,45 +197,6 @@ export class ConfiguracoesAlunoComponent implements OnInit {
   fechar() {
     //this.close();
     window.history.back();
-  }
-
-  loadPersonal(): Observable<ConfigAgenda> {
-    return this.personalService.getMe().pipe(
-      map(personal => ({
-        diasAtendimento: [0,1,2,3,4,5,6].filter(i => (personal as any)[`dia${i}`]),
-        horaInicio: personal.hora_inicio,
-        horaFim: personal.hora_fim,
-        intervaloMinutos: personal.intervalo_minutos,
-        mostrarLocal: personal.mostrarLocal,
-        mostrarServico: personal.mostrarServico,
-        mostrarEquipto: personal.mostrarEquipto,
-        servicoid:personal.servicoid
-      }))
-    );
-  }
-
-  gerarHorasPossiveis(inicio: number, fim: number, intervaloMin: number): string[] {
-
-    const horas: string[] = [];
-    const date = new Date();
-    date.setHours(inicio, 0, 0, 0);
-
-    const end = new Date();
-    end.setHours(fim, 0, 0, 0);
-
-    while (date <= end) {
-      const hora = date.getHours().toString().padStart(2, '0');
-      const minuto = date.getMinutes().toString().padStart(2, '0');
-      horas.push(`${hora}:${minuto}`);
-      date.setMinutes(date.getMinutes() + intervaloMin);
-    }
-
-      console.log('inicio:', inicio);
-      console.log('fim:', fim);
-      console.log('intervaloMin:', intervaloMin);
-console.log('horas possíveis:', horas);
-    return horas;
-
   }
 
   get diasAulaArray(): FormArray  {
